fc4796d43acd673121379e77b625c855b2ab36ac
Tashdik <s2110477153@ru.ac.bd>
2024-05-10 17:40:48 +0600

    post operation done

--- index.js
+++ index.js
@@ -5,15 +5,23 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+    // optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 app.use(express.json());
 
 
 
 
 
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iepmiic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iepmiic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://WorkWave:Lok7Qk7Jz7hcpAhu@cluster0.iepmiic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,13 +37,26 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
+        const postCollections = client.db("WorkWave").collection("posts");
 
+        app.get('/post', async(req, res) =>{
+            const result = await postCollections.find().toArray();
+
+            res.send(result);
+        })
+
+        app.post('/post', async (req, res) => {
+            const data = req.body;
+            // console.log(data);
+            const result = await postCollections.insertOne(data);
+            res.send(result);
+        })
 
 
 
 
         // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
+        await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
