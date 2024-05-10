c5a8b63fdfcdee9181c007e7f71c49dd85ad394c
Tashdik <s2110477153@ru.ac.bd>
2024-05-10 20:38:33 +0600

    fetch api by id  operation complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -48,8 +48,19 @@ async function run() {
 
         app.post('/post', async (req, res) => {
             const data = req.body;
-            console.log(data);
+            // console.log(data);
             const result = await postCollections.insertOne(data);
+            res.send(result);
+        })
+
+        // get single post by ID
+        app.get('/post/:id', async(req, res) =>{
+            const id = req.params.id;
+            // console.log(id);
+            const query ={ _id : new ObjectId(id)}
+            const result = await postCollections.findOne(query);
+            
+
             res.send(result);
         })
 
