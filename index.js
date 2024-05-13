109986e05ae2069a7d6f96fc3e059b247d222e0d
Tashdik <s2110477153@ru.ac.bd>
2024-05-14 00:52:41 +0600

    server deployed successfully

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://workwave-a11.web.app', 'https://workwave-a11.firebaseapp.com'],
     credentials: true,
     // optionSuccessStatus: 200,
 }
@@ -292,7 +292,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
